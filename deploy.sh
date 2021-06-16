49ad85e397a8338ad36ccff95c79a499ea54bc56
snayp <snayp@yandex.ru>
2021-06-16 08:44:05 +0400

    rr548333-555

--- deploy.sh
+++ deploy.sh
@@ -15,7 +15,6 @@ git push -u origin
 
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
-git rm -rf public/
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
